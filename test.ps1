d56c79ba750edb0bf67f17eaacd8bfa185d9cef6
saru1604 <103305844+saru1604@users.noreply.github.com>
2022-05-22 16:49:38 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-cd: D:\ci-cd\ci-cd
-cd: D:\ci-cd\ci-cd\posts\build-net-5-github-actions
+cd: D:/docu/ci-cd
+cd: D:/docu/ci-cd/posts/build-net-5-github-actions
 git clone https://github.com/saru1604/ci-cd.git
 dotnet build
 dotnet pack
